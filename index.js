13dc1f2416718f0086a4c585759c34f6f8786d22
peteryeungtt@gmail.com <peteryeungtt@gmail.com>
2023-05-16 17:51:49 -0400

    Correct screen error in js file

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-let burger = document.querySelector(".navigator");
+let burger = document.querySelector('.navigator');
 let body = document.body;
-let navbar = document.querySelector(".navbar");
-let newMenu = document.querySelector(".hiddenMenu");
-let xButton = document.querySelector(".xIcon");
-let portfolio = document.querySelector(".portfolioL");
-let about = document.querySelector(".aboutL");
-let contact = document.querySelector(".contactL");
+let navbar = document.querySelector('.navbar');
+let newMenu = document.querySelector('.hiddenMenu');
+let xButton = document.querySelector('.xIcon');
+let portfolio = document.querySelector('.portfolioL');
+let about = document.querySelector('.aboutL');
+let contact = document.querySelector('.contactL');
 
-burger.addEventListener("click", ()=>{
-  if (window.screen.availWidth < 768){
-    newMenu.style.display= "flex";
-    body.style.overflow= "hidden";
+burger.addEventListener('click', () => {
+  if (window.matchMedia('(min-width: 786px)')) {
+    newMenu.style.display = 'flex';
+    body.style.overflow = 'hidden';
   }
 });
 
-xButton.addEventListener("click", () =>{
-  body.style.overflow = "auto";
-  newMenu.style.display = "none";
+xButton.addEventListener('click', () => {
+  body.style.overflow = 'auto';
+  newMenu.style.display = 'none';
 });
 
-portfolio.addEventListener("click", ()=>{
-  body.style.overflow = "auto";
-  newMenu.style.display = "none";
-  window.scrollBy({ 
-    top: 870, 
-    left: 0, 
-    behavior: 'smooth' 
+portfolio.addEventListener('click', () => {
+  body.style.overflow = 'auto';
+  newMenu.style.display = 'none';
+  window.scrollBy({
+    top: 870,
+    left: 0,
+    behavior: 'smooth',
   });
 });
 
-about.addEventListener("click", ()=>{
-  body.style.overflow = "auto";
-  newMenu.style.display = "none";
-  window.scrollBy({ 
-    top: 3400, 
-    left: 0, 
-    behavior: 'smooth' 
+about.addEventListener('click', () => {
+  body.style.overflow = 'auto';
+  newMenu.style.display = 'none';
+  window.scrollBy({
+    top: 3400,
+    left: 0,
+    behavior: 'smooth',
   });
 });
 
-contact.addEventListener("click", ()=>{
-  body.style.overflow = "auto";
-  newMenu.style.display = "none";
-  window.scrollBy({ 
-    top: 5000, 
-    left: 0, 
-    behavior: 'smooth' 
+contact.addEventListener('click', () => {
+  body.style.overflow = 'auto';
+  newMenu.style.display = 'none';
+  window.scrollBy({
+    top: 5000,
+    left: 0,
+    behavior: 'smooth',
   });
 });
