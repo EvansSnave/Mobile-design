b163c3f904a1689a91c82ac1055c42711157b6f0
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-05-19 15:46:13 -0500

    changes

--- js/popUpWindows.js
+++ js/popUpWindows.js
@@ -18,42 +18,94 @@ const cards = [
     subtitle: ['Facebook','Full Stack Dev','2015'],
   },
   {
-    name: 'Tonic',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    name: 'Facebook 360',
+    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     FeaturedImage: 'Assests/Snapshoot\ Portfolio.svg',
-    technologies: ['HTML','CSS','Javascript'],
+    technologies: ['HTML','Ruby on rails','CSS','Javascript'],
     liveVersion: 'https://example.com',
     source: 'https://example.com',
-    subtitle: ['Facebook','Back End Dev','2015'],
+    subtitle: ['Facebook','Full Stack Dev','2015'],
+  },
+  {
+    name: 'Uber navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    FeaturedImage: 'Assests/Snapshoot\ Portfolio.svg',
+    technologies: ['HTML','Ruby on rails','CSS','Javascript'],
+    liveVersion: 'https://example.com',
+    source: 'https://example.com',
+    subtitle: ['Uber','Lead Developer','2018'],
   },
 ];
 
-const PopUp=(card)=> {
+const popUpCard=(card)=> {
   let workSection = document.querySelector('.worksSection');
-  let cardContainer = document.querySelector('.cardContainer');
-  const technologies = cards[card].technologies.map((x) => `<li>${x}</li>`);
-  let subtitles = cards[card].subtitle.map(x => `<li>${x}</li>`).join('');
-  let Html = `
+  let technologies = cards[card].technologies.map((x) => `<li>${x}</li>`).join('');
+  const subtitles = cards[card].subtitle.map(x => `<li>${x}</li>`);
+  let Html1 = `
     <div class="space"></div>
     <div class="firstCard">
       <div class="snapshot snapshots"></div>
       <div class="section2">
         <h3 class="tonic">${cards[card].name}</h3>
         <ul class="canopyList">
-          <li class="canopy">${technologies[0]}</li>
+          <li class="canopy">${subtitles[0]}</li>
           <li class="counter"><img src="Assests/Countercounter.svg"></img></li>
-          <li class="backEnd">${technologies[1]}</li>
+          <li class="backEnd">${subtitles[1]}</li>
           <li class="counter"><img src="Assests/Countercounter.svg"></img></li>
-          <li class="year">${technologies[2]}</li>
+          <li class="year">${subtitles[2]}</li>
         </ul>
-        <p class="description">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
-        <div class="works">
-          <button class="htmlButton buttons"></button>
-          <button class="cssButton buttons"></button>
-          <button class="jsButton buttons"></button>
+        <p class="description">${cards[card].description}</p>
+        <ul class="works">
+          ${technologies}
+        </ul>
+        <div class="btns">
+          <a href="${cards[card].liveVersion}" class="btn">See live</a>
+          <a href="${cards[card].source}" class="btn">See source</a>
         </div>
-        <button class="seeProject">See project</button></div>
+        <button class="seeProject">See project</button>
+      </div>
     </div>
     <div class="space"></div>
   `;
+  workSection.insertAdjacentHTML('afterbegin', Html1);
 }
+
+const createSection = ()=>{
+  let section = document.querySelector(".worksSection");
+  for (let card = cards.length -1; card >= 0; card -=1){
+    let technologies = cards[card].technologies.map((x) => `<li class="buttons">${x}</li>`).join('');
+    const subtitles = cards[card].subtitle.map(x => `<li>${x}</li>`);
+    let created = `
+    <div class="space"></div>
+    <div class="firstCard">
+      <div class="snapshot snapshots"></div>
+      <div class="section2">
+        <h3 class="tonic">${cards[card].name}</h3>
+        <ul class="canopyList">
+          <li class="canopy">${subtitles[0]}</li>
+          <li class="counter"><img src="Assests/Countercounter.svg"></img></li>
+          <li class="backEnd">${subtitles[1]}</li>
+          <li class="counter"><img src="Assests/Countercounter.svg"></img></li>
+          <li class="year">${subtitles[2]}</li>
+        </ul>
+        <p class="description">${cards[card].description}</p>
+        <ul class="works">
+          ${technologies}
+        </ul>
+        <button class="seeProject">See project</button>
+      </div>
+    </div>
+    <div class="space"></div>
+    `;
+    section.insertAdjacentHTML('afterbegin', created);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  createSection();
+  document.querySelectorAll('.worksSection .btn').forEach((btn, card) => {
+    btn.addEventListener('click', () => {
+      popUpCard(card);
+    });
+  });
+});
